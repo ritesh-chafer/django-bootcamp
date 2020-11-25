b90babbebe2522100e2930b595d16a08eae8a2c4
indranil2507 <iindranilsaha1@gmail.com>
2020-11-26 02:57:39 +0530

    Decreasing code use

--- tutorial/blog/views.py
+++ tutorial/blog/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
  
- 
- 
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'November 27, 2018'
+    },
+    {
+        'author': 'Chafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'November 26, 2020'
+
+    }
+]
+
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
