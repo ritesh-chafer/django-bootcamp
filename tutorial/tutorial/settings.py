8b446e9b31797944fff39e8488a577721057acb6
ritesh-chafer <riteshprasadsingh8@gmail.com>
2021-01-27 23:33:18 +0530

    views.py

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'users.apps.RegistrationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
